5a1970628487320b68732f148c06624d33398db6
gunotham <gunthamsolanki@gmail.com>
2025-07-21 22:56:38 +0530

    Implemented login controller for registration and login

---
+++ src/main/java/com/extron/MyFirstECom/Controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.extron.MyFirstECom.Controller;
+
+import com.extron.MyFirstECom.Model.Users;
+import com.extron.MyFirstECom.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<Users> registerUser(@RequestBody Users user){
+        Users newUser = service.registerUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> userLogin(@RequestBody Users user){
+        return new ResponseEntity<>(service.verifyUser(user), HttpStatus.OK);
+    }
+}
